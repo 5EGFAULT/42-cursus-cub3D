2afca4f40b6cc65b1773bdf979cf9c97fae00099
Souinia Ayoub <souiniaayoub@gmail.com>
2022-06-28 18:29:21 +0100

    move and rotate

--- src/game/hooks.c
+++ src/game/hooks.c
@@ -6,7 +6,7 @@
 /*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 22:00:31 by asouinia          #+#    #+#             */
-/*   Updated: 2022/06/28 16:25:49 by asouinia         ###   ########.fr       */
+/*   Updated: 2022/06/28 18:25:38 by asouinia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,51 @@ int	key_hook(int keycode, t_game *game)
 
 void	move_player(t_game *game, int keycode)
 {
-	int	pas;
+	int		pas;
+	double	movedir[2];
+	int		correct_key;
+	double	rotdeg;
 
-	pas = 2;
+	pas = 5;
+	correct_key = 0;
 	if (keycode == KEY_UP)
-		game->pos[1] += -pas;
+	{
+		game->pos[0] += pas * game->dir[0];
+		game->pos[1] += pas * game->dir[1];
+		correct_key = 1;
+	}
 	else if (keycode == KEY_DOWN)
-		game->pos[1] += pas;
-	else if (keycode == KEY_LEFT)
-		game->pos[0] += -pas;
-	else if (keycode == KEY_RIGHT)
-		game->pos[0] += pas;
-	render_map2d(game);
-	draw_player(game);
-	draw_rays(game);
+	{
+		game->pos[0] -= pas * game->dir[0];
+		game->pos[1] -= pas * game->dir[1];
+		correct_key = 1;
+	}
+	else if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
+	{
+		correct_key = 1;
+		if (keycode == KEY_LEFT)
+			rotdeg = 0.01745329251 * -90;
+		else
+			rotdeg = 0.01745329251 * 90;
+		movedir[0] = game->dir[0] * cos(rotdeg) - game->dir[1] * sin(rotdeg);
+		movedir[1] = game->dir[0] * sin(rotdeg) + game->dir[1] * cos(rotdeg);
+		if (keycode == KEY_LEFT)
+		{
+			game->pos[0] += 1 * movedir[0];
+			game->pos[1] += 1 * movedir[1];
+		}
+		else
+		{
+			game->pos[0] += 1 * movedir[0];
+			game->pos[1] += 1 * movedir[1];
+		}
+	}
+	if (correct_key)
+	{
+		render_map2d(game);
+		draw_player(game);
+		draw_rays(game);
+	}
 }
 
 void	rotate_player(t_game *game, int keycode)
